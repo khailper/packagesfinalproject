a13fe7c7a172c4a7a866bb99ed86b5ce3fe5bd47
khailper <khailper@gmail.com>
2016-12-08 15:33:00 -0600

    fixed expected result from test

--- tests/basictest.R
+++ tests/basictest.R
@@ -1,4 +1,4 @@
 #check fars_summarize_years returns a list
 library(testthat)
 library(packagesfinalproject)
-expect_that(fars_summarize_years(2013), is_a("list"))
+expect_that(fars_summarize_years(2013), is_a("data.frame"))
